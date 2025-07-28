97f3730d28eb05a39395af656ba79a1446e1e98d
Maria Koczwara <mkoczwara@poczta.onet.pl>
2025-07-28 13:53:38 +0200

    Fix port value

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, '/client')));
 
-app.listen(process.env.PORT || 8000, () => {
-    console.log(`Server is running on port: ${process.env.PORT ? process.env.PORT : 8000}`);
+app.listen(8000, () => {
+    console.log('Server is running on port: 8000');
 });
